eb82cfd98c9c22f35e6ef7e3088122bc88857546
bruhhhz <23520258@gm.uit.edu.vn>
2025-12-18 13:52:06 +0700

    giao dienclient

--- LabTest/CustomerClient.Designer.cs
+++ LabTest/CustomerClient.Designer.cs
@@ -28,12 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textip = new System.Windows.Forms.TextBox();
+            this.textport = new System.Windows.Forms.TextBox();
+            this.butketnot = new System.Windows.Forms.Button();
+            this.butmenu = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textsomon = new System.Windows.Forms.TextBox();
+            this.combsoban = new System.Windows.Forms.TextBox();
+            this.combomon = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butthoat = new System.Windows.Forms.Button();
+            this.butdatmon = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textip
+            // 
+            this.textip.Location = new System.Drawing.Point(90, 40);
+            this.textip.Name = "textip";
+            this.textip.Size = new System.Drawing.Size(138, 20);
+            this.textip.TabIndex = 0;
+            // 
+            // textport
+            // 
+            this.textport.Location = new System.Drawing.Point(234, 40);
+            this.textport.Name = "textport";
+            this.textport.Size = new System.Drawing.Size(100, 20);
+            this.textport.TabIndex = 1;
+            // 
+            // butketnot
+            // 
+            this.butketnot.Location = new System.Drawing.Point(340, 37);
+            this.butketnot.Name = "butketnot";
+            this.butketnot.Size = new System.Drawing.Size(75, 23);
+            this.butketnot.TabIndex = 2;
+            this.butketnot.Text = "Kết nối";
+            this.butketnot.UseVisualStyleBackColor = true;
+            // 
+            // butmenu
+            // 
+            this.butmenu.Location = new System.Drawing.Point(340, 122);
+            this.butmenu.Name = "butmenu";
+            this.butmenu.Size = new System.Drawing.Size(75, 23);
+            this.butmenu.TabIndex = 3;
+            this.butmenu.Text = "MENU!";
+            this.butmenu.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(421, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(367, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textsomon
+            // 
+            this.textsomon.Location = new System.Drawing.Point(90, 177);
+            this.textsomon.Name = "textsomon";
+            this.textsomon.Size = new System.Drawing.Size(44, 20);
+            this.textsomon.TabIndex = 5;
+            // 
+            // combsoban
+            // 
+            this.combsoban.Location = new System.Drawing.Point(90, 151);
+            this.combsoban.Name = "combsoban";
+            this.combsoban.Size = new System.Drawing.Size(44, 20);
+            this.combsoban.TabIndex = 6;
+            // 
+            // combomon
+            // 
+            this.combomon.FormattingEnabled = true;
+            this.combomon.Location = new System.Drawing.Point(90, 124);
+            this.combomon.Name = "combomon";
+            this.combomon.Size = new System.Drawing.Size(138, 21);
+            this.combomon.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Chọn món";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Số bàn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Số món";
+            // 
+            // butthoat
+            // 
+            this.butthoat.Location = new System.Drawing.Point(646, 196);
+            this.butthoat.Name = "butthoat";
+            this.butthoat.Size = new System.Drawing.Size(75, 23);
+            this.butthoat.TabIndex = 11;
+            this.butthoat.Text = "Thoát!";
+            this.butthoat.UseVisualStyleBackColor = true;
+            // 
+            // butdatmon
+            // 
+            this.butdatmon.Location = new System.Drawing.Point(481, 196);
+            this.butdatmon.Name = "butdatmon";
+            this.butdatmon.Size = new System.Drawing.Size(75, 23);
+            this.butdatmon.TabIndex = 12;
+            this.butdatmon.Text = "Đặt";
+            this.butdatmon.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "IP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(231, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "PORT";
+            // 
+            // CustomerClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.butdatmon);
+            this.Controls.Add(this.butthoat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combomon);
+            this.Controls.Add(this.combsoban);
+            this.Controls.Add(this.textsomon);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.butmenu);
+            this.Controls.Add(this.butketnot);
+            this.Controls.Add(this.textport);
+            this.Controls.Add(this.textip);
+            this.Name = "CustomerClient";
             this.Text = "CustomerClient";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textip;
+        private System.Windows.Forms.TextBox textport;
+        private System.Windows.Forms.Button butketnot;
+        private System.Windows.Forms.Button butmenu;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textsomon;
+        private System.Windows.Forms.TextBox combsoban;
+        private System.Windows.Forms.ComboBox combomon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butthoat;
+        private System.Windows.Forms.Button butdatmon;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
